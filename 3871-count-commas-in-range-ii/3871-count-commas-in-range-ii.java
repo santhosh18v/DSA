4ca75bfc2a4b1495a9bf1c0dec47b2971f115d4c
Santhosh Reddy <thungasanthoshkumarreddy@gmail.com>
2026-09-09 23:16:30 +0530

    Time: 1 ms (99.36%), Space: 42.7 MB (37.18%) - LeetHub

--- 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.java
+++ 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.java
@@ -1,20 +1,28 @@
 class Solution {
     public long countCommas(long n) {
-        if(n<1000){
-            return 0;
+
+        long count = 0;
+
+        if (n >= 1000) {
+            count += n - 999;
         }
-        else if(n<1000000){
-            return n-999;
+
+        if (n >= 1000000) {
+            count += n - 999999;
         }
-        else if(n<1000000000){
-            return ((n-999999)+(n-999));
+
+        if (n >= 1000000000L) {
+            count += n - 999999999L;
         }
-        else if(n<1000000000000L){
-            return ((n-999999999)+(n-999999)+(n-999));
+
+        if (n >= 1000000000000L) {
+            count += n - 999999999999L;
         }
-        else if(n<1000000000000000L){
-            return((n-999999999999L)+(n-999999999)+(n-999999)+(n-999));
+
+        if (n >= 1000000000000000L) {
+            count += n - 999999999999999L;
         }
-        return((n-999999999999999L)+(n-999999999999L)+(n-999999999)+(n-999999)+(n-999));
+
+        return count;
     }
 }
