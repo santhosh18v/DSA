2445a7fcd4a290f160de860a33e01a07c89ae13c
Santhosh Reddy <thungasanthoshkumarreddy@gmail.com>
2026-09-23 23:34:19 +0530

    Time: 3 ms (26.98%), Space: 43.5 MB (35.69%) - LeetHub

---
+++ 1796-second-largest-digit-in-a-string/1796-second-largest-digit-in-a-string.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int secondHighest(String s) {
+        int largest=-1;
+        int secondLargest=-1;
+        HashSet<Integer> set=new HashSet<>();
+        for(int i=0;i<s.length();i++){
+            if(Character.isDigit(s.charAt(i))){
+                int digit=s.charAt(i)-'0';
+                if(set.contains(digit)){
+                    continue;
+
+                }
+                set.add(digit);
+                if(digit>largest){
+                    secondLargest=largest;
+                    largest=digit;
+                }
+                if(digit<largest && digit>secondLargest){
+                    secondLargest=digit;
+                }
+            }
+        }
+        return secondLargest;
+    }
+}
